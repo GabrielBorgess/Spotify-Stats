745848875e222d57f8347026367df8eec72633b2
Gabriel-Borges <staffgabs@gmail.com>
2024-01-07 22:15:32 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-900 min-h-screen flex justify-center text-center flex-col'>
+     <div className=''>
+        <h1 className='font-sans font-bold text-6xl text-white'>Spotify Stats</h1>
+        <button className='pt-6 text-white'>Get my Spotify Stats</button>
+     </div>
+    </div>
+  )
+}
+
+export default App
