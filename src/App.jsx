bc6903d844bc2c910686520e0ebd89e7b5b2ccf9
Gabriel-Borges <staffgabs@gmail.com>
2024-01-08 08:27:03 -0300

    tentando arrumar a requisicao

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './index.css'
 
 function App() {
+  const getSpotifyUser = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/login', {
+        mode: 'no-cors'
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao autenticar com o Spotify');
+      }
+      
+      const data = await response.json()
+      console.log(data)
+      
+    } catch (error) {
+      console.log(error)
+      throw(error)
+    }
+  }
 
   return (
-    <div className='bg-gray-900 min-h-screen flex justify-center text-center flex-col'>
+    <div className='bg-gradient-to-b from-blue-900 via-blue-950 to-gray-900 min-h-screen flex justify-center text-center flex-col'>
      <div className=''>
-        <h1 className='font-sans font-bold text-6xl text-white'>Spotify Stats</h1>
-        <button className='pt-6 text-white'>Get my Spotify Stats</button>
+        <h1 className='font-sans font-bold text-7xl text-white'>Spotify Stats</h1>
+        <button onClick={getSpotifyUser} className='m-8 p-4 text-white border-2 w-1/5 rounded-lg'>Get my Spotify Stats</button>
      </div>
     </div>
   )
