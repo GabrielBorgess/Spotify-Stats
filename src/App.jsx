eca7cc2cb3f3811214deb0ab1e735f8be6690325
Gabriel-Borges <staffgabs@gmail.com>
2024-01-10 19:36:01 -0300

    pegando infomacoes da api, por enquanto so os top artistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,93 @@
 import './index.css'
 
 function App() {
-  const getSpotifyUser = async () => {
-      window.open('http://localhost:8080/login', 'Spotify Authorization', 'width=600,height=600');
+  const clientId = "b127e89b86d2450c99868c6ab0de314c";
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+
+  const fetchData = async () => {
+    if(!code){
+      redirectToAuthCodeFlow(clientId)
+    } else {
+      try {
+        const accessToken = await getAccessToken(clientId, code);
+        const userProfile = await getUserProfile(accessToken);
+        console.log(userProfile)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  const redirectToAuthCodeFlow = async (clientId) => {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("scope", "user-read-private user-read-email user-top-read");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`
+  };
+
+  const generateCodeVerifier = (length) => {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  const generateCodeChallenge = async (codeVerifier) => {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+  };
+
+  const getAccessToken = async (clientId, code) => {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("code_verifier", verifier);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
+  };
+
+  const getUserProfile = async (token) => {
+    const result = await fetch("https://api.spotify.com/v1/me/top/artists", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
   };
 
   return (
     <div className='bg-gradient-to-b from-blue-900 via-blue-950 to-gray-900 min-h-screen flex justify-center text-center flex-col'>
      <div className=''>
         <h1 className='font-sans font-bold text-7xl text-white'>Spotify Stats</h1>
-        <button onClick={getSpotifyUser} className='m-8 p-4 text-white border-2 w-1/5 rounded-lg'>Get my Spotify Stats</button>
+        <button onClick={fetchData} className='m-8 p-4 text-white border-2 w-1/5 rounded-lg'>Get my Spotify Stats</button>
      </div>
     </div>
   )
